1f35fdfffc8367198a92ea6a1d3c886398bd3890
VaishnaviKubade <116034012+VaishnaviKubade@users.noreply.github.com>
2022-10-18 01:43:11 +0530

    Add files via upload

---
+++ 2.Data Types in C.c
@@ -0,0 +1,54 @@
+/*#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=10,b=1;
+    int c;
+    c=a+b;
+    printf("Sum is: %d",c);
+    printf("\nSum of %i and %i is: %i",a,b,c);
+    getch();
+}*/
+
+/*#include<stdio.h>
+void main()
+{
+    float a=8.5;
+    float b=2.5;
+    float c=a*b;
+    printf("Multiplication of %f and %f is:%f",a,b,c);
+}*/
+
+/*include<stdio.h>
+void main()
+{
+    char gender='F';
+    printf("Gender is:%c",gender);
+}*/
+
+/*#include<stdio.h>
+void main()
+{
+    int p=19;
+    char g='F';
+    float w=59.9;
+    printf("I am %i years old, gender is %c and weight is %f",p,g,w);
+}*/
+
+/*#include<stdio.h>
+void main()
+{
+    unsigned char a=257;
+    printf("%i",a);
+}*/
+
+/*#include<stdio.h>
+void main()
+{
+    char a=5,b=10,c=15;
+    char d=a+b+c;
+    printf("Sum is:%d",d);
+}*/
+
+
+

2570f7c0aa0d1044faacd45cc807df1cf7ef2f8c
VaishnaviKubade <116034012+VaishnaviKubade@users.noreply.github.com>
2022-10-28 12:03:00 +0530

    Add files via upload

---
+++ 9.1.Hello 5 times.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+/*void main()
+{
+    //Without using for loop
+    printf("Hello");
+    printf("\nHello");
+    printf("\nHello");
+    printf("\nHello");
+    printf("\nHello");
+    getch();
+}*/
+
+void main()
+{
+    //Using for loop
+    int i;
+    for(i=1;i<=5;i++)
+        printf("Hello\n");
+    getch();
+}

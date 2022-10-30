d712b6c6d21b8bdfc91dd7855f61385095466b6c
VaishnaviKubade <116034012+VaishnaviKubade@users.noreply.github.com>
2022-10-30 12:43:30 +0530

    Add files via upload

---
+++ 15.1.goto example.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+/*void main()
+{
+    int i=5;
+    printf("Hello\n");
+    printf("Hi\n");
+    printf("How are you?\n");
+    if(i=5)
+        goto end;
+    printf("Hope you liked the course!\n");
+    printf("Hope you complete it soon\n");
+    printf("Have fun\n");
+    printf("Charge your laptop\n");
+
+    end:
+        printf("Bye Bye!!\n");
+}*/
+
+void main()
+{
+    int i=5;
+    printf("Hello\n");
+    printf("Hi\n");
+    printf("How are you?\n");
+    if(i==4)
+        goto end;
+    printf("Hope you liked the course!\n");
+    printf("Hope you complete it soon\n");
+    printf("Have fun\n");
+    printf("Charge your laptop\n");
+
+    end:
+        printf("Bye Bye!!\n");
+}
+
